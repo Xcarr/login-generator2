94e91d25af73355eebe0cb614b885fa37fc9faac
Xcarr <xavier.carrau@u-tlse3.fr>
2018-11-22 14:49:01 +0100

    fix #3

--- src/test/java/geco/LoginGeneratorTest.java
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -16,4 +16,19 @@ public class LoginGeneratorTest {
     public void generateLoginForNomAndPrenom() {
         // On verifie que l'ajout de Paul Durand soit ajouté de la forme PDUR
     }
+
+    @Test
+    public void CT1(){
+        //on vérifie que l'ajout de John Ralling soit mentionné en "JRAL2"
+    }
+
+    @Test
+    public void CT2(){
+        //génération de Jean Rolling donne "JROL1"
+    }
+
+    @Test
+    public void CT3(){
+        //génération de Paul Dùrand donne "PDUR"
+    }
 }
