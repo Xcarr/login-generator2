aafe6a8722da0b22852e4c406c6673784f292536
Xcarr <xavier.carrau@univ-tlse3.fr>
2018-11-28 16:50:57 +0100

    fix 4

--- src/test/java/geco/LoginServiceTest.java
+++ src/test/java/geco/LoginServiceTest.java
@@ -2,43 +2,27 @@ package geco;
 
 import org.junit.Test;
 
-<<<<<<< HEAD
-/*import static org.junit.Assert.*;*/
-=======
 import static org.junit.Assert.*;
->>>>>>> origin/master
 
 public class LoginServiceTest {
 
     @Test
     public void loginExists() {
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
     }
 
     @Test
     public void addLogin() {
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
     }
 
     @Test
     public void findAllLoginsStartingWith() {
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
     }
 
     @Test
     public void findAllLogins() {
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/master
     }
 }
