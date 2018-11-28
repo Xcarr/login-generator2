2e1268fc3af6f8147bc02eae3081da8a817cb44e
Xcarr <xavier.carrau@univ-tlse3.fr>
2018-11-28 16:38:22 +0100

    commit password second

---
+++ src/main/java/geco/PasswordGeneration.java
@@ -0,0 +1,13 @@
+package geco;
+
+public class PasswordGeneration {
+    public String generate() {
+        String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"; // Tu supprimes les lettres dont tu ne veux pas
+        String pass = "";
+        for (int x = 0; x < 8; x++) {
+            int i = (int) Math.floor(Math.random() * 62); // Si tu supprimes des lettres tu diminues ce nb
+            pass += chars.charAt(i);
+        }
+        return pass;
+    }
+}
